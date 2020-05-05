b90484921f858a0abda25e07f4a785fd89e07d20
mgondan <Matthias.Gondan@psy.ku.dk>
2020-05-05 10:01:39 +0200

    favicon

--- server.pl
+++ server.pl
@@ -95,6 +95,11 @@ page(Id) :-
 	    integrity('sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh'),
 	    crossorigin(anonymous)
 	  ]),
+	link(
+          [ rel(icon),
+	    href('/favicon.ico'),
+	    type('image/x-icon')
+	  ]),
 	meta([name(viewport), content('width=device-width, initial-scale=1')])
       ],
       [ \item(Id, Response),
