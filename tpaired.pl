851aa8a781e490f5538dda410ba3fe062953ecb5
mgondan <Matthias.Gondan@psy.ku.dk>
2020-05-15 09:11:30 +0200

    overline for the averages

--- tpaired.pl
+++ tpaired.pl
@@ -7,6 +7,10 @@
 :- consult(temp).
 :- use_module(r).
 
+mathml:math_hook(Flags, m_D, Flags, overline('D')).
+mathml:math_hook(Flags, m_T0, Flags, overline("T0")).
+mathml:math_hook(Flags, m_EOT, Flags, overline("EOT")).
+
 mathml:math_hook(Flags, s_D, Flags, sub(s, 'D')).
 mathml:math_hook(Flags, s_T0, Flags, sub(s, "T0")).
 mathml:math_hook(Flags, s_EOT, Flags, sub(s, "EOT")).
@@ -15,20 +19,20 @@ mathml:math_hook(Flags, s_EOT, Flags, sub(s, "EOT")).
 % Paired t-test, t-ratio
 %
 :- multifile item/1.
-item(tpaired: paired_tratio('D', ['T0', 'EOT'], s_D, [s_T0, s_EOT], 'N', mu)).
+item(tpaired: paired_tratio(m_D, [m_T0, m_EOT], s_D, [s_T0, s_EOT], 'N', mu)).
 
 :- multifile intermediate/1.
 intermediate(tpaired: paired_tratio/6).
 
 :- multifile item//1.
 item(tpaired: Response) -->
-    { D <- 'D',
+    { D <- m_D,
       Mu <- mu,
       S_D <- s_D,
       N <- 'N',
-      T0 <- 'T0',
+      T0 <- m_T0,
       S_T0 <- s_T0,
-      EOT <- 'EOT',
+      EOT <- m_EOT,
       S_EOT <- s_EOT,
       Tails <- tails,
       Alpha <- alpha,
@@ -138,25 +142,25 @@ buggy(tpaired: denominator, From >> To, Flags, Feed, Trap) :-
     Feed = [ "Please use the standard deviation of the change scores ",
              \mml(Flags, S_D), " for the ", 
              span(class('text-nowrap'), [\mml(Flags, t), "-ratio"]),
-	     " instead of ", 
-	     span(class('text-nowrap'), [\mml(Flags, color(sd, S)), "."])
-	   ],
+	         " instead of ", 
+	         span(class('text-nowrap'), [\mml(Flags, color(sd, S)), "."])
+	       ],
     Trap = [ "Use the standard deviation of the change scores in the ", 
              span(class('text-nowrap'), [\mml(Flags, t), "-ratio "]),
-	     "instead of ", 
-	     span(class('text-nowrap'), 
+	         "instead of ", 
+	         span(class('text-nowrap'), 
 	         [\mml(Flags, list((' ', "or", ' '), S_wrong)), "."]) 
-	   ].
+	       ].
 
 % Choose t-ratio for one-sample t-test
 expert(tpaired: paired_t, From >> To, Flags, Feed, Hint) :-
     From = paired_t(D, Mu, S, N),
     To   = tratio(dfrac(D - Mu, S / sqrt(N))),
-    Feed = ["Correctly identified the formula for the ",
-            span(class('text-nowrap'), [\mml(Flags, t), "-ratio for paired samples."])],
-    Hint = ["Determine the ",
-            span(class('text-nowrap'), [\mml(Flags, t), "-ratio"]), " ",
-            \mml(Flags, dfrac(D - Mu, S / sqrt(N))), "."].
+    Feed = [ "Correctly identified the formula for the ",
+             span(class('text-nowrap'), [\mml(Flags, t), "-ratio for paired samples."])],
+    Hint = [ "Determine the ",
+             span(class('text-nowrap'), [\mml(Flags, t), "-ratio"]), " ",
+             \mml(Flags, dfrac(D - Mu, S / sqrt(N))), "."].
 
 % Choose t-ratio for two-groups t-test
 buggy(tpaired: groups_t, From >> To, Flags, Feed, Trap) :-
@@ -164,13 +168,13 @@ buggy(tpaired: groups_t, From >> To, Flags, Feed, Trap) :-
     Inst = groups_t(T0, S_T0, N, EOT, S_EOT, N),
     Of   = paired_t(D, Mu, S, N),
     To   = instead_of(groups_t, Inst, Of),
-    Feed = ["The result matches a ",
-            span(class('text-nowrap'), [\mml(Flags, t), "-test"]), " for independent ",
-            "groups. Please use the ",
-            span(class('text-nowrap'), [\mml(Flags, t), "-test"]), " for paired ",
-            "samples instead."],
-    Trap = ["Do not use the ", span(class('text-nowrap'), [\mml(Flags, t), "-test "]), 
-	    "for independent groups in this problem with paired measurements."].
+    Feed = [ "The result matches a ",
+             span(class('text-nowrap'), [\mml(Flags, t), "-test"]), " for independent ",
+             "groups. Please use the ",
+             span(class('text-nowrap'), [\mml(Flags, t), "-test"]), " for paired ",
+             "samples instead."],
+    Trap = [ "Do not use the ", span(class('text-nowrap'), [\mml(Flags, t), "-test "]), 
+	         "for independent groups in this problem with paired measurements."].
 
 intermediate(tpaired: groups_t/6).
 
@@ -179,11 +183,12 @@ expert(tpaired: groups_t, From >> To, Flags, Feed, Hint) :-
     From = groups_t(M_A, S_A, N_A, M_B, S_B, N_B),
     Pool = denoting(subsup(s, "pool", 2), var_pool(S_A^2, N_A, S_B^2, N_B), "the pooled variance"),
     To   = tratio(dfrac(M_A - M_B, sqrt(Pool * (1/N_A + 1/N_B)))),
-    Feed = ["Correctly applied the expression for the ",
-            span(class('text-nowrap'), [\mml(Flags, t), "-ratio for independent samples."])],
-    Hint = ["Determine the ",
-            span(class('text-nowrap'), [\mml(Flags, t), "-ratio"]), " ",
-            \mml(Flags, dfrac(M_A - M_B, sqrt(Pool * (1/N_A + 1/N_B))))].
+    Feed = [ "Correctly applied the expression for the ",
+             span(class('text-nowrap'), [\mml(Flags, t), "-ratio for independent samples."])],
+    Hint = [ "Determine the ",
+             span(class('text-nowrap'), [\mml(Flags, t), "-ratio"]), " ",
+             \mml(Flags, dfrac(M_A - M_B, sqrt(Pool * (1/N_A + 1/N_B))))
+           ].
 
 % Forgot school math
 buggy(tpaired: school, From >> To, Flags, Feed, Trap) :-
@@ -191,45 +196,58 @@ buggy(tpaired: school, From >> To, Flags, Feed, Trap) :-
     dif(A, B),
     Inst = frac(1, A + B),
     To   = instead_of(school, Inst, From),
-    Feed = ["Please remember school math: ", 
-            \mml(Flags, color(school, color("black", From) \= color("black", Inst)))],
-    Trap = ["Please remember school math: ",
-            \mml(Flags, From = frac(A + B, A*B))].
+    Feed = [ "Please remember school math: ", 
+             \mml(Flags, color(school, color("black", From) \= color("black", Inst)))
+           ],
+    Trap = [ "Please remember school math: ",
+             \mml(Flags, From = frac(A + B, A*B))
+           ].
 
 buggy(tpaired: school, From >> To, Flags, Feed, Trap) :-
     From = 1/A + 1/A,
     Inst = frac(1, 2*A),
     To   = instead_of(school, Inst, From),
-    Feed = ["Please remember school math: ",
-             \mml(Flags, color(school, color("black", From) \= color("black", Inst)))],
+    Feed = [ "Please remember school math: ",
+             \mml(Flags, color(school, color("black", From) \= color("black", Inst)))
+           ],
     Trap = [ "Please remember school math: ",
-             \mml(Flags, From = frac(2, A))].
+             \mml(Flags, From = frac(2, A))
+           ].
 
 % Forget parentheses
 buggy(tpaired: frac_paren, From >> To, Flags, Feed, Trap) :-
     From = dfrac(A - B, C / D),
     To   = left_landed(frac_paren, A - right_landed(frac_paren, 
                dfrac(left_elsewhere(frac_paren, A - B), 
-	             right_elsewhere(frac_paren, C / D)) / D)),
-    Feed = ["Please check the parentheses around the numerator and the ",
-            "denominator of the fraction ",
-            \mml(Flags, dfrac(color(frac_paren, paren(black(A - B))), 
-	                color(frac_paren, paren(black(C / D))))), "."],
+	               right_elsewhere(frac_paren, C / D)) / D)),
+    Feed = [ "Please check the parentheses around the numerator and the ",
+             "denominator of the fraction ",
+             span(class('text-nowrap'), 
+               [ \mml(Flags, dfrac(color(frac_paren, paren(black(A - B))), 
+	             color(frac_paren, paren(black(C / D))))), "."
+               ])
+           ],
     Trap = Feed.
 
 buggy(tpaired: paren_num, From >> To, Flags, Feed, Trap) :-
     From = dfrac(A - B, C / D),
     To   = left_landed(paren_num, A - dfrac(left_elsewhere(paren_num, A - B), C / D)),
-    Feed = ["Please check the parentheses around the numerator of the fraction ",
-            \mml(Flags, dfrac(color(paren_num, paren(black(A - B))), C / D)), "."],
+    Feed = [ "Please check the parentheses around the numerator of the fraction ",
+             span(class('text-nowrap'), 
+               [ \mml(Flags, dfrac(color(paren_num, paren(black(A - B))), C / D)), "."
+               ])
+           ],
     Trap = Feed.
 
 buggy(tpaired: paren_denom, From >> To, Flags, Feed, Trap) :-
     From = dfrac(A - B, C / D),
     To   = right_landed(paren_denom, dfrac(A - B, right_elsewhere(paren_denom, C / D)) / D),
-    Feed = ["Please check the parentheses around the denominator of the ",
-            "fraction ", 
-	    \mml(Flags, dfrac(A - B, color(paren_denom, paren(black(C / D))))), "."],
+    Feed = [ "Please check the parentheses around the denominator of the ",
+             "fraction ", 
+             span(class('text-nowrap'), 
+               [ \mml(Flags, dfrac(A - B, color(paren_denom, paren(black(C / D))))), "."
+               ])
+           ],
     Trap = Feed.
 
 % Forget square root
@@ -237,8 +255,11 @@ buggy(tpaired: root, From >> To, Flags, Feed, Trap) :-
     From = sqrt(N),
     Inst = N,
     To   = instead_of(root, Inst, From),
-    Feed = ["Please do not forget the square root around ", 
-            \mml([highlight(all) | Flags], color(root, N)), "."],
+    Feed = [ "Please do not forget the square root around ", 
+             span(class('text-nowrap'), 
+               [ \mml([highlight(all) | Flags], color(root, N)), "."
+               ])
+           ],
     Trap = Feed.
 
 % Forget to subtract the mu from the null hypothesis
@@ -246,55 +267,56 @@ buggy(tpaired: mu, From >> To, Flags, Feed, Trap) :-
     From = dfrac(D - mu, S / SQRTN),
     To   = dfrac(omit_right(mu, D - mu), S / SQRTN),
     Feed = [ "Please do not forget to subtract the average change ", 
-             \mml(Flags, color(mu, mu)), " under the null hypothesis." ],
+             \mml(Flags, color(mu, mu)), " under the null hypothesis." 
+           ],
     Trap = Feed.
 
 r_init(tpaired) :-
     r_init,
     {|r||
-    paired_t <- function(d, mu, s, n)
-    {
-        dfrac(d - mu, s / sqrt(n))
-    }
+        paired_t <- function(d, mu, s, n)
+        {
+            dfrac(d - mu, s / sqrt(n))
+        }
 
-    var_pool <- function(var_A, n_A, var_B, n_B)
-    {
-        frac((n_A - 1) * var_A + (n_B - 1) * var_B, n_A + n_B - 2)
-    }
+        var_pool <- function(var_A, n_A, var_B, n_B)
+        {
+            frac((n_A - 1) * var_A + (n_B - 1) * var_B, n_A + n_B - 2)
+        }
 
-    tpaired_data <- function(seed)
-    {
-        set.seed(seed)
-	N    = sample(25:40, size=1)
-	Id   = 1:N
-	X    = rnorm(N, mean=25, sd=5)
-	T0   = round(X + rnorm(N, mean=0, sd=4))
-	EOT  = round(X + rnorm(N, mean=-6, sd=5))
-	D    = T0 - EOT
-	data.frame(Id, T0, EOT, D)    
-    }
+        tpaired_data <- function(seed)
+        {
+            set.seed(seed)
+            N    = sample(25:40, size=1)
+            Id   = 1:N
+            X    = rnorm(N, mean=25, sd=5)
+            T0   = round(X + rnorm(N, mean=0, sd=4))
+            EOT  = round(X + rnorm(N, mean=-6, sd=5))
+            D    = T0 - EOT
+            data.frame(Id, T0, EOT, D)    
+        }
 
-    data  = tpaired_data(seed=4711)
-    N     = nrow(data)
-    T0    = round(mean(data$T0), 1)
-    s_T0  = round(sd(data$T0), 1)
-    EOT   = round(mean(data$EOT), 1)
-    s_EOT = round(sd(data$EOT), 1)
-    D     = round(mean(data$T0 - data$EOT), 1)
-    s_D   = round(sd(data$T0 - data$EOT), 1)
-    mu    = 5
-    alpha = 0.05
-    tails = 'two-tailed'
+        data  = tpaired_data(seed=4711)
+        N     = nrow(data)
+        m_T0  = round(mean(data$T0), 1)
+        s_T0  = round(sd(data$T0), 1)
+        m_EOT = round(mean(data$EOT), 1)
+        s_EOT = round(sd(data$EOT), 1)
+        m_D   = round(mean(data$T0 - data$EOT), 1)
+        s_D   = round(sd(data$T0 - data$EOT), 1)
+        mu    = 5
+        alpha = 0.05
+        tails = 'two-tailed'
 
-    # Exam 2018-06ag
-    N = 24
-    T0 = 24.0
-    s_T0 = 5.1
-    EOT = 18.3
-    s_EOT = 4.7
-    D = 5.8
-    s_D = 3.8
-    mu = 4.0
+        # Exam 2018-06ag
+        N     = 24
+        m_T0  = 24.0
+        s_T0  = 5.1
+        m_EOT = 18.3
+        s_EOT = 4.7
+        m_D   = 5.8
+        s_D   = 3.8
+        mu    = 4.0
     |},
     csvfile(tpaired, data).
 
@@ -361,3 +383,4 @@ example :-
     Topic = tpaired,
     buggies(Topic, Bugs),
     writeln(bugs: Bugs).
+
