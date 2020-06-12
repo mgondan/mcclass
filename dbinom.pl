368c574d4ee2b5d9a61b4a0d380b7835fa36e570
mgondan <matthias.gondan@psy.ku.dk>
2020-06-12 19:31:24 +0000

    stub for binomial density

---
+++ dbinom.pl
@@ -0,0 +1,232 @@
+% Binomial density
+:- use_module(relevant).
+:- use_module(intermediate).
+:- use_module(library(mathml)).
+:- use_module(library(http/html_write)).
+:- consult(html).
+:- consult(temp).
+:- use_module(r).
+
+mathml:math_hook(Flags, m_D, Flags, overline('D')).
+mathml:math_hook(Flags, m_T0, Flags, overline("T0")).
+mathml:math_hook(Flags, m_EOT, Flags, overline("EOT")).
+
+mathml:math_hook(Flags, s_D, Flags, sub(s, 'D')).
+mathml:math_hook(Flags, s_T0, Flags, sub(s, "T0")).
+mathml:math_hook(Flags, s_EOT, Flags, sub(s, "EOT")).
+
+%    
+% Binomial density
+%
+:- multifile item/1.
+item(dbinom: paired_tratio(m_D, [m_T0, m_EOT], s_D, [s_T0, s_EOT], 'N', mu)).
+
+:- multifile intermediate/1.
+intermediate(dbinom: paired_tratio/6).
+
+:- multifile item//2.
+item(dbinom, Response) -->
+    { D <- m_D,
+      Mu <- mu,
+      S_D <- s_D,
+      N <- 'N',
+      T0 <- m_T0,
+      S_T0 <- s_T0,
+      EOT <- m_EOT,
+      S_EOT <- s_EOT,
+      Tails <- tails,
+      Alpha <- alpha,
+      maplist(mathml, ["HDRS", "T0", "EOT", 'D'], H),
+      maplist([X, Y] >> mathml(round1(X), Y), ["Mean", T0, EOT, D], R1),
+      maplist([X, Y] >> mathml(round1(X), Y), ["SD", S_T0, S_EOT, S_D], R2)
+    }, 
+    html(
+      [ div(class(card), div(class('card-body'),
+          [ h1(class('card-title'), "Phase II clinical study"),
+            p(class('card-text'), 
+            [ "Consider a clinical study on rumination-focused Cognitive ", 
+              "Behavioral Therapy (rfCBT) with ", \mml('N' = round0(N)), 
+              " patients. The primary outcome is the score on the ",
+              "Hamilton Rating Scale for Depression (HDRS, range from ",
+              "best = 0 to worst = 42). The significance level is set to ", 
+              \mml(alpha = round('100%'(Alpha))), " ", \mml(Tails), "."
+	        ]),
+            \table(H, [R1, R2])
+	        % \download(tpaired)
+	      ])),
+        div(class(card), div(class('card-body'),
+          [ h4(class('card-title'), [a(id(question), []), "Question"]),
+            p(class('card-text'), 
+              [ "Does rfCBT lead to a relevant reduction (i.e., more than ",
+	            \mml(mu = Mu), " units) in mean HDRS scores between ",
+                "baseline (T0) and End of Treatment (EOT)?" 
+              ]),
+            \question(question, 
+                response, 
+                ["Please determine the ", \nowrap([\mml(t), "-ratio."])], 
+                Response)
+	      ]))
+      ]).
+
+% Correctly identify as a paired t-test
+:- multifile expert/5.
+expert(dbinom: paired_tratio, From >> To, Flags, Feed, Hint) :-
+    From = paired_tratio(D, M_wrong, S, S_wrong, N, Mu),
+    To   = paired_tratio_2(D, M_wrong, S, S_wrong, N, Mu),
+    Feed = [ "Correctly identified the problem as a ",
+             \nowrap([\mml(Flags, t), "-test"]), " for paired samples." 
+           ],
+    Hint = [ "This is a ",
+             \nowrap([\mml(Flags, t), "-test"]), " for paired samples." 
+           ].
+
+intermediate(dbinom: paired_tratio_2/6).
+
+% Choose correct numbers for numerator
+expert(dbinom: numerator, From >> To, Flags, Feed, Hint) :-
+    From = paired_tratio_2(D, _, S, S_wrong, N, Mu),
+    To   = paired_tratio_3(D, S, S_wrong, N, Mu),
+    Feed = [ "Correctly identified the numerator of the ",
+             \nowrap([\mml(Flags, t), "-ratio."]) 
+           ],
+    Hint = [ "The numerator is ", \nowrap([\mml(Flags, D - Mu), "."]) ].
+
+intermediate(dbinom: paired_tratio_3/5).
+
+% Choose correct numbers for denominator
+expert(dbinom: denominator, From >> To, Flags, Feed, Hint) :-
+    From = paired_tratio_3(D, S_D, _, N, Mu),
+    To   = paired_t(D, Mu, S_D, N),
+    Feed = [ "Correctly identified the denominator of the ",
+             \nowrap([\mml(Flags, t), "-ratio."]) 
+           ],
+    Hint = [ \mml(Flags, S_D), " is used in the denominator of the ",
+             \nowrap([\mml(Flags, t), "-ratio."]) 
+           ].
+
+intermediate(dbinom: paired_t/4).
+
+expert(dbinom: paired_t, From >> To, Flags, Feed, Hint) :-
+    From = paired_t(D, Mu, S, N),
+    To   = tratio(dfrac(D - Mu, S / sqrt(N)), N - 1),
+    Feed = [ "Correctly identified the formula for the ",
+             \nowrap([\mml(Flags, t), "-ratio for paired samples."])
+           ],
+    Hint = [ "Determine the ", \nowrap([\mml(Flags, t), "-ratio"]), " ",
+             \nowrap([\mml(Flags, dfrac(D - Mu, S / sqrt(N))), "."])
+           ].
+
+:- multifile r_init/1.
+r_init(dbinom) :-
+    r_init,
+    {|r||
+        paired_t <- function(d, mu, s, n)
+        {
+            dfrac(d - mu, s / sqrt(n))
+        }
+
+        var_pool <- function(var_A, n_A, var_B, n_B)
+        {
+            frac((n_A - 1) * var_A + (n_B - 1) * var_B, n_A + n_B - 2)
+        }
+
+        tpaired_data <- function(seed)
+        {
+            set.seed(seed)
+            N    = sample(25:40, size=1)
+            Id   = 1:N
+            X    = rnorm(N, mean=25, sd=5)
+            T0   = round(X + rnorm(N, mean=0, sd=4))
+            EOT  = round(X + rnorm(N, mean=-6, sd=5))
+            D    = T0 - EOT
+            data.frame(Id, T0, EOT, D)    
+        }
+
+        data  = tpaired_data(seed=4711)
+        N     = nrow(data)
+        m_T0  = round(mean(data$T0), 1)
+        s_T0  = round(sd(data$T0), 1)
+        m_EOT = round(mean(data$EOT), 1)
+        s_EOT = round(sd(data$EOT), 1)
+        m_D   = round(mean(data$T0 - data$EOT), 1)
+        s_D   = round(sd(data$T0 - data$EOT), 1)
+        mu    = 5
+        alpha = 0.05
+        tails = 'two-tailed'
+
+        # Exam 2018-06ag
+        N     = 24
+        m_T0  = 24.0
+        s_T0  = 5.1
+        m_EOT = 18.3
+        s_EOT = 4.7
+        m_D   = 5.8
+        s_D   = 3.8
+        mu    = 4.0
+    |},
+    csvfile(dbinom, data).
+
+%
+% Invoke example
+%
+:- multifile example/0.
+example :-
+    Topic = dbinom,
+    r_init(Topic),
+    item(Topic: Item),
+    solution(Topic, Item, Solution, Path),
+    writeln(solution: Solution),
+    r(Solution, Result),
+    writeln(result: Result),
+    writeln(path: Path),
+    mathml(Solution = number(Result), Mathml),
+    writeln(mathml: Mathml),
+    hints(Topic, Item, Path, _, Hints),
+    writeln(hints: Hints),
+    traps(Topic, Item, Path, _, Traps),
+    writeln(traps: Traps),
+    praise(Topic, Item, Path, _, Praise),
+    writeln(praise: Praise).
+
+example :-
+    Topic = dbinom,
+    r_init(Topic),
+    item(Topic: Item),
+    wrong(Topic, Item, Wrong, Woodden),
+    writeln(wrong: Wrong),
+    mathex(fix, Wrong, Fix),
+    writeln(fix: Fix),
+    mathex(show, Wrong, Show),
+    writeln(show: Show),
+    r(Wrong, Result),
+    writeln(result: Result),
+    writeln(path: Woodden),
+    palette(Wrong, Flags),
+    mathml([highlight(all) | Flags], Wrong \= Result, Mathml),
+    writeln(mathml: Mathml),
+    feedback(Topic, Item, Woodden, Flags, _, Feedback),
+    writeln(feedback: Feedback),
+    praise(Topic, Item, Woodden, Code_Praise, Praise),
+    writeln(praise: Praise),
+    mistakes(Topic, Item, Woodden, Flags, Code_Mistakes, Mistakes),
+    writeln(mistakes: Mistakes),
+    solution(Topic, Item, _, Path),
+    hints(Topic, Item, Path, Code_Hints, _),
+    relevant(Code_Praise, Code_Hints, Rel_Praise, Irrel_Praise),
+    writeln(relevant_praise: Rel_Praise),
+    writeln(irrelevant_praise: Irrel_Praise),
+    traps(Topic, Item, Path, Code_Traps, _),
+    relevant(Code_Mistakes, Code_Traps, Rel_Mistakes, Irrel_Mistakes),
+    writeln(relevant_mistakes: Rel_Mistakes),
+    writeln(irrelevant_mistakes: Irrel_Mistakes).
+
+example :-
+    Topic = dbinom,
+    html(\item(Topic, ''), HTML, []),
+    print_html(HTML).
+
+example :-
+    Topic = dbinom,
+    buggies(Topic, Bugs),
+    writeln(bugs: Bugs).
+
