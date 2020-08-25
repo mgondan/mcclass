67e8bcf768f4bf7ddf88fd18940926212d536315
mgondan <matthias.gondan@psy.ku.dk>
2020-08-25 10:37:27 +0000

    New bootstrap

--- server.pl
+++ server.pl
@@ -135,9 +135,9 @@ page(Id) :-
     reply_html_page(
       [ title('McClass'),
         link([rel(stylesheet),
-            href('https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
-	        integrity('sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk'),
-	        crossorigin(anonymous)]),
+                href('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'),
+                integrity('sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z'),
+                crossorigin(anonymous)]),
 	    link([rel(icon), href('/mcclass/favicon.ico'), type('image/x-icon')]),
 	    meta([name(viewport), content('width=device-width, initial-scale=1')])
       ],
