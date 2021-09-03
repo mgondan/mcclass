7c52101148b68d3be246648525c1e2be243b0a22
vhaenel <90041497+vhaenel@users.noreply.github.com>
2021-09-03 15:55:53 +0200

    stages

--- tpaired.pl
+++ tpaired.pl
@@ -1,7 +1,9 @@
-:- module(tpaired, [start/2, expert/4, buggy/4, render//3]).
+% tpaired.pl
+
+:- module(tpaired, [start/2, expert/5, buggy/5, render//3]).
 
 :- use_module(library(http/html_write)).
-:- multifile start/2, expert/4, buggy/4, render//3.
+:- multifile start/2, expert/5, buggy/5, render//3.
 
 render(tpaired, item(T0, S_T0, EOT, S_EOT, D, S_D, N, Mu), Form) -->
     { RD = 4.5,
@@ -51,7 +53,7 @@ render(tpaired, item(T0, S_T0, EOT, S_EOT, D, S_D, N, Mu), Form) -->
 % Prolog warns if the rules of a predicate are not adjacent. This would not
 % help us here, so the definitions for intermediate, expert and buggy are
 % declared to be discontiguous.
-:- discontiguous intermediate/2, expert/4, buggy/4.
+:- discontiguous intermediate/2, expert/5, buggy/5.
 
 % t-test for paired samples
 intermediate(_, item).
@@ -60,13 +62,13 @@ start(tpaired, item(t0, s_t0, eot, s_eot, d, s_d, n, mu)).
 % First step: Extract the correct information for a paired t-test from the task
 % description
 intermediate(tpaired, paired).
-expert(tpaired, X, Y, [name(paired)]) :-
+expert(stage(2), tpaired, X, Y, [name(paired)]) :-
     X = item(_, _, _, _, D, S_D, N, Mu),
     Y = paired(D, Mu, S_D, N).
 
 % Second step: Apply the formula for the t-ratio. dfrac/2 is a fraction in
 % "display" mode (a bit larger font than normal)
-expert(tpaired, X, Y, [name(tratio)]) :-
+expert(stage(2), tpaired, X, Y, [name(tratio)]) :-
     X = paired(D, Mu, S_D, N),
     Y = dfrac(D - Mu, S_D / sqrt(N)).
 
@@ -78,21 +80,21 @@ expert(tpaired, X, Y, [name(tratio)]) :-
 % tend to see an improvement even in the absence of any therapeutical effect.
 % This misconception is even built into SPSS, because the paired samples t-test
 % in SPSS only allows for mu = 0. 
-buggy(tpaired, X, Y, [bug(mu)]) :-
+buggy(stage(2), tpaired, X, Y, [bug(mu)]) :-
     X = paired(D, Mu, S_D, N),
     Y = dfrac(omit_right(D - Mu), S_D / sqrt(N)).
 
 % Misconception: Run the t-test for independent samples despite the correlated
 % measurements.
 intermediate(indep).
-buggy(tpaired, X, Y, [bug(indep)]) :-
+buggy(stage(2), tpaired, X, Y, [bug(indep)]) :-
     X = item(T0, S_T0, EOT, S_EOT, _, _, N, _),
     Y = indep(T0, S_T0, N, EOT, S_EOT, N).
 
 % This step is used to determine the test statistic for the t-test for
 % independent samples. The step itself is correct, although it is only needed
 % if a wrong decision has been made before [bug(indep)].
-expert(tpaired, X, Y, [name(tratio)]) :-
+expert(stage(2), tpaired, X, Y, [name(tratio)]) :-
     X = indep(T0, S_T0, N, EOT, S_EOT, N),
     P = var_pool(S_T0^2, N, S_EOT^2, N),
     Y = dfrac(T0 - EOT, sqrt(P * (1/N + 1/N))).
@@ -106,13 +108,13 @@ expert(tpaired, X, Y, [name(tratio)]) :-
 % everyone falls into this trap at least once, including me and the student
 % assistants. I have coined it "bug(school)", since it is an example in which
 % the person has forgotten school math.
-buggy(tpaired, X, Y, [bug(school)]) :-
+buggy(stage(2), tpaired, X, Y, [bug(school)]) :-
     dif(N1, N2),
     X = 1/N1 + 1/N2,
     Y = frac(1, N1 + N2).
 
 % Same for N1 = N2
-buggy(tpaired, X, Y, [bug(school)]) :-
+buggy(stage(2), tpaired, X, Y, [bug(school)]) :-
     X = 1/N + 1/N,
     Y = frac(1, 2*N).
 
@@ -121,7 +123,7 @@ buggy(tpaired, X, Y, [bug(school)]) :-
 % 
 % This is the first buggy rule that ever came to my attention, therefore the
 % name, bug1.
-buggy(tpaired, X, Y, [bug(bug1)]) :-
+buggy(stage(2), tpaired, X, Y, [bug(bug1)]) :-
     X = dfrac(D - Mu, S / SQRT_N),
     Y = D - dfrac(Mu, S) / SQRT_N.
 
@@ -131,21 +133,21 @@ buggy(tpaired, X, Y, [bug(bug1)]) :-
 % 
 % The depends means: This bug is limited to the paired t-test and co-occurs
 % with s_t0 (these flags are ignored, but will be activated in later versions).
-buggy(tpaired, X, Y, [bug(d_t0), depends(s_t0), depends(paired)]) :-
+buggy(stage(1), tpaired, X, Y, [bug(d_t0), depends(s_t0), depends(paired)]) :-
     X = d,
     Y = t0.
 
 % Use SD of T0 instead of SD of D
-buggy(tpaired, X, Y, [bug(s_t0), depends(d_t0), depends(paired)]) :-
+buggy(stage(1), tpaired, X, Y, [bug(s_t0), depends(d_t0), depends(paired)]) :-
     X = s_d,
     Y = s_t0.
 
 % Use mean EOT instead of mean D
-buggy(tpaired, X, Y, [bug(d_eot), depends(s_eot), depends(paired)]) :-
+buggy(stage(1), tpaired, X, Y, [bug(d_eot), depends(s_eot), depends(paired)]) :-
     X = d,
     Y = eot.
 
 % Use SD of EOT instead of SD of D
-buggy(tpaired, X, Y, [bug(s_eot), depends(d_eot), depends(paired)]) :-
+buggy(stage(1), tpaired, X, Y, [bug(s_eot), depends(d_eot), depends(paired)]) :-
     X = s_d,
     Y = s_eot.
