27f5322015fb5f3d67d56b23e91cd6a0eb34fd50
vhaenel <90041497+vhaenel@users.noreply.github.com>
2021-09-03 15:53:18 +0200

    stages

--- steps.pl
+++ steps.pl
@@ -1,22 +1,22 @@
-:- module(steps, [step/4]).
+:- module(steps, [step/5]).
 
 :- use_module(tasks).
 
 % Apply expert rule
-step(Task, X, Y, Flags) :-
-    expert(Task, X, Y, Flags).
+step(Stage, Task, X, Y, Flags) :-
+    expert(Stage, Task, X, Y, Flags).
 
 % Apply buggy rule
-step(Task, X, Y, Flags) :-
-    buggy(Task, X, Y, Flags).
+step(Stage, Task, X, Y, Flags) :-
+    buggy(Stage, Task, X, Y, Flags).
 
 % Enter term and apply rule to components. For example, enter 
 % dfrac(Numerator, Denominator) and check if a rule can be applied to the
 % numerator and/or the denominator.
-step(Task, X, Y, Flags) :-
+step(Stage, Task, X, Y, Flags) :-
     compound(X),
     compound_name_arguments(X, Name, XArgs),
     nth1(Index, XArgs, Arg, Rest),
-    step(Task, Arg, New, Flags),
+    step(Stage, Task, Arg, New, Flags),
     nth1(Index, YArgs, New, Rest),
     compound_name_arguments(Y, Name, YArgs).
