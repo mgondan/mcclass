f3661de291a8ff7bfc6334db61560ae48e605c7d
mgondan <Matthias.Gondan@psy.ku.dk>
2021-10-15 22:45:18 +0200

    improved rendering of omit_left

--- mathml.pl
+++ mathml.pl
@@ -1263,14 +1263,20 @@ math(Flags, omit_left(_Bug, Expr), New, M),
 
 math(Flags, omit_left(bug(Bug), Expr), New, M),
     option(error(fix), Flags, highlight)
- => Expr =.. [Op, L, R],
-    Flags = New,
+ => ( math(Flags, Expr, New, Expr1)
+     -> Expr1 =.. [Op, L, R]
+      ; Expr =.. [Op, L, R],
+        Flags = New
+    ),
     M = list(space, [color(Bug, box(color("#000000", list(space, [L, sign(Op)])))), R]).
 
 math(Flags, omit_left(bug(Bug), Expr), New, M),
     option(error(highlight), Flags, highlight)
- => Expr =.. [Op, L, R],
-    Flags = New,
+ => ( math(Flags, Expr, New, Expr1)
+     -> Expr1 =.. [Op, L, R]
+      ; Expr =.. [Op, L, R],
+        Flags = New
+    ),
     M = list(space, [color(Bug, cancel(color("#000000", list(space, [L, sign(Op)])))), R]).
 
 math(Flags, omit_right(_Bug, Expr), New, M),
@@ -1291,7 +1297,8 @@ math(Flags, omit_right(bug(Bug), Expr), New, M),
     option(error(highlight), Flags, highlight)
  => ( math(Flags, Expr, New, Expr1)
      -> Expr1 =.. [Op, L, R]
-      ; Expr =.. [Op, L, R], Flags = New
+      ; Expr =.. [Op, L, R], 
+        Flags = New
     ),
     M = list(space, [L, color(Bug, cancel(color("#000000", list(space, [sign(Op), R]))))]).
 
