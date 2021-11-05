ea9b0a26b3e0742640d9828edb2570b882e62e71
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-05 22:18:11 +0100

    task specific session information

--- session.pl
+++ session.pl
@@ -1,15 +1,21 @@
+% Session management help functions for web and console applications
 :- module(session, 
     [ session_id/1,
-      session_assert/1, 
+      session_assert/1,
+      session_assert/2,
       session_data/1,
+      session_data/2,
       session_retract/1,
-      session_tmpfile/1,
-      session_id/1]).
+      session_retract/2,
+      session_tmpfile/1
+    ]).
 
 :- use_module(library(http/http_session)).
-:- dynamic session_data/1.
 
-% Session management for web and console applications.
+:- dynamic session_data/1.
+:- dynamic session_data/2.
+
+% Session management for web and console applications
 session_id(Id) :-
     http_in_session(S),
     !,
@@ -20,28 +26,38 @@ session_id(default_session).
 % Store information for a session, either in the session or as a dynamic 
 % predicate
 session_assert(Data) :-
+    session_assert(no_task, Data).
+
+% Task-specific session information
+session_assert(Task, Data) :-
     http_in_session(_),
     !,
-    http_session_assert(Data).
+    http_session_assert(session_data(Task, Data)).
 
-session_assert(Data) :-
-    assert(session_data(Data)).
+session_assert(Task, Data) :-
+    assert(session_data(Task, Data)).
 
-% Retrieve information. If no http session is running, the dynamic predicate 
-% session_data/1 is used.
+% Retrieve information. If no http session is running, the dynamic predicate
+% session_data/1,2 is used.
 session_data(Data) :-
+    session_data(no_task, Data).
+
+session_data(Task, Data) :-
     http_in_session(_),
     !,
-    http_session_data(Data).
+    http_session_data(session_data(Task, Data)).
 
 % Remove information
 session_retract(Data) :-
+    session_retract(no_task, Data).
+
+session_retract(Task, Data) :-
     http_in_session(_),
     !,
-    http_session_retract(Data).
+    http_session_retract(session_task(Task, Data)).
 
-session_retract(Data) :-
-    retract(session_data(Data)).
+session_retract(Task, Data) :-
+    retract(session_data(Task, Data)).
 
 % Create a session-specific temporary file
 session_tmpfile(String) :-
