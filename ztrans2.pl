9cc3d0b51b62cedda51a09bbfa3323ac83d397c9
Vidris <93139438+Vidris@users.noreply.github.com>
2021-11-11 12:58:21 +0100

    Update ztrans2.pl

--- ztrans2.pl
+++ ztrans2.pl
@@ -1,1 +1,49 @@
+:- use_module(library(http/html_write)).
+:- use_module(session).
+:- use_module(table).
+:- use_module(r).
+:- use_module(mathml).
 
+:- multifile init/1, data/1, data/2, start/2, intermediate/2, expert/5, buggy/5, feedback/5, hint/5, render//3.
+
+init(ztrans2) :-
+    r_session_source(ztrans2).
+
+mathml:hook(Flags, x, Flags, 'X').
+
+interval:hook(pl, x, r(x)).
+interval:hook(pl, sigma, r(sigma)).
+interval:hook(pl, z, r(z)).
+interval:hook(pl, pnorm(Z), r(pnorm(Z))).
+interval:hook(pl, p, r(p)).
+
+render(ztrans2, item(X, Mu, Sigma), Form) -->
+    { option(resp(R), Form, '#.##') },
+    html(
+      [ div(class(card), div(class('card-body'),
+        [ h1(class('card-title'), "Normal distribution"),
+          p(class('card-text'), 
+            [ "Let ", \mmlm([round(0)], X), " follow a Normal distribution with ",
+              "expectation ", \mmlm([round(0)], Mu = r(mu)), " and ",
+              "standard deviation ", \mmlm([round(0)], [Sigma = r(sigma), "."]),
+              "A table of the standard ",
+              "Normal distribution is found below."
+            ])
+        ])),
+        div(class(card), div(class('card-body'),
+          [ h4(class('card-title'), [a(id(question), []), "Question"]),
+            p(class('card-text'),
+              [ "How many realizations are ",
+                  "below ", \mmlm([round(0)], [r(x), "?"])
+              ]),
+            form([class(form), method('POST'), action('#ztrans2-response')],
+              [ div(class("input-group mb-3"),
+                  [ div(class("input-group-prepend"), 
+                      span(class("input-group-text"), "Response")),
+                    input([class("form-control"), type(text), name(resp), value(R)]),
+                      div(class("input-group-append"),
+                        button([class('btn btn-primary'), type(submit)], "Submit"))
+                  ])
+              ])
+          ]))
+      ]).
