f0a32a50821cc0626dc32b10f2846862bf1d7aca
Vidris <93139438+Vidris@users.noreply.github.com>
2021-11-27 12:55:56 +0100

    Update ztrans2.pl
    added pdecimal, pdecimal2 and zx bugs.
    
    Still get negInfin results in 15 of the 47 bugs, needs to be looked into
    When p is larger than 10 an server overflow error occurs, needs to be looked into

--- ztrans2.pl
+++ ztrans2.pl
@@ -33,8 +33,7 @@ render(ztrans2, item(P, Mu, Sigma), Form) -->
         div(class(card), div(class('card-body'),
           [ h4(class('card-title'), [a(id(question), []), "Question"]),
             p(class('card-text'),
-              [ "How many realizations are ",
-                  "below ", \mmlm([round(0)], [r(p), "%?"])
+              [ "In which area do the upper ", \mmlm([round(0)], [r(p), "% fall?"])
               ]),
             form([class(form), method('POST'), action('#ztrans2-response')],
               [ div(class("input-group mb-3"),
@@ -60,7 +59,7 @@ start(ztrans2, item(p, mu, sigma)) :-
 intermediate(ztrans2, qnorm_).
 expert(ztrans2, stage(2), From, To, [step(expert, allinone, [])]) :-
     From = item(P, Mu, Sigma),
-    To = { '<-'( z, qnorm_(P)) ;
+    To = { '<-'( z, qnorm_(P/100)) ;
 	   '<-'(x, z * Sigma + Mu) ;
            x
          }.
@@ -70,6 +69,7 @@ feedback(ztrans2, allinone, [], _Col, FB) :-
 
 hint(ztrans2, allinone, [], _Col, FB) :-
     FB = [ "Try to do everything correctly."].
+
 
 % Expert rule (correct tail)
 expert(ztrans2, stage(2), From, To, [step(expert, correct_tail, [P])]) :-
@@ -82,6 +82,7 @@ feedback(ztrans2, correct_tail, [_P], _Col, FB) :-
 hint(ztrans2, correct_tail, [_P], _Col, FB) :-
     FB = [ "The lower tail of the Normal distribution is used." ].
 
+
 % Buggy rule (wrong tail) The wrong tail of the normal distribution was selected.
 buggy(ztrans2, stage(2), From, To, [step(buggy, wrong_tail, [P])]) :-
     From = qnorm_(P),
@@ -92,6 +93,7 @@ feedback(ztrans2, wrong_tail, [_P], _Col, FB) :-
 
 hint(ztrans2, wrong_tail, [_P], _Col, FB) :-
     FB = [ "Do not use the lower tail of the Normal distribution." ].
+
 
 % Buggy Rule (swap) Mu and Sigma were swapped.
 buggy(ztrans2, stage(1), From, To, [step(buggy, swap, [mu, sigma])]) :-
@@ -108,13 +110,50 @@ hint(ztrans2, swap, [Mu, Sigma], Col, FB) :-
     FB = [ "Try using ", \mmlm(Col, color(swap, Mu)), " and ", 
 	   \mmlm(Col, color(swap, Sigma)), " in a different configuration." ].
 
+
 % Buggy Rule (vardev swap) standard deviation was mistaken with variance.
-buggy(ztrans2, stage(1), From, To, [step(buggy, vardev_swap, [sigma])]) :-
+buggy(ztrans2, stage(1), From, To, [step(buggy, vardev_swap, [])]) :-
     From = item(p, mu, sigma),
     To = item(p, mu, sigma^2).
 
-feedback(ztrans2, vardev_swap, [sigma], Col, FB) :-
-    FB = [ \mmlm(Col, color(vardev_swap, sigma)), "was squared incorrectly." ].
+feedback(ztrans2, vardev_swap, [], Col, FB) :-
+    FB = [ \mmlm(Col, color(vardev_swap, sigma)), "was squared by mistake" ].
 
-hint(ztrans2, vardev_swap, [sigma], _Col, FB) :-
+hint(ztrans2, vardev_swap, [], _Col, FB) :-
     FB = [ "Use the standard deviation instead of the variance." ].
+
+
+% Buggy Rule (pdecimal) p was taken to be a tenth of its true value (5% -/-> 0,05. 5% --> 0,005).
+buggy(ztrans2, stage(1), From, To, [step(buggy, pdecimal, [])]) :-
+    From = item( p , X , Y ),
+    To = item( p/10 , X , Y ).
+
+feedback(ztrans2, pdecimal, [], _Col , FB) :-
+    FB = [ "P-% was incorrectly converted to a decimal representation." ].
+
+hint(ztrans2, pdecimal, [], Col, FB) :-
+    FB = [ \mmlm(Col, color(pdecimal, r(p))), "% in decimal representation is ", \mmlm(Col, color(pdecimal, r(p/100))) ].
+
+
+% Buggy Rule (pdecimal2) p was taken to be ten times its true value (5% -/-> 0,05. 5% --> 0,5).
+buggy(ztrans2, stage(1), From, To, [step(buggy, pdecimal2, [])]) :-
+    From = item( p , X , Y ),
+    To = item( p*10, X, Y ).
+
+feedback(ztrans2, pdecimal2, [], _ , FB) :-
+    FB = [ "P-% was incorrectly converted to a decimal representation." ].
+
+hint(ztrans2, pdecimal2, [], Col, FB) :-
+    FB = [ \mmlm(Col, color(pdecimal2, r(p))), "% in decimal representation is ", \mmlm(Col, color(pdecimal2, r(p/100))) ].
+
+
+% Buggy Rule (zx) The z value was calculated but taken to be the endresult.
+buggy(ztrans2, stage(2), From, To, [step(buggy, zx, [z, Sigma, Mu])]) :-
+    From = z * Sigma + Mu,
+    To = instead(bug(zx), z , z * Sigma + Mu).
+
+feedback(ztrans2, zx, [], _Col, FB) :-
+    FB = [ "To complete the exercise successfully you have to do the second calculation aswell." ].
+
+hint(ztrans2, zx, [z, Sigma, Mu], Col, FB) :-
+    FB = [ \mmlm(Col, color(zx, z)), "is the correct answer of the first equation. To continue calculate ", \mmlm(Col, color(zx, z * Sigma + Mu)), "." ].
