4a79d6ba6c520642a24035f3549a95e8e7cb7e4c
mgondan <Matthias.Gondan@psy.ku.dk>
2022-09-05 00:30:36 +0200

    only expert rules in wrong branch of instead

--- tgroups.pl
+++ tgroups.pl
@@ -209,8 +209,8 @@ hint(school, [A, B], Col, FB)
 
 % Forgot square root around the denominator
 buggy(stage(2), From, To, [step(buggy, sqrt1, [S2P * Ns])]) :-
-    From = dfrac(Num, sqrt(S2P * Ns)),
-    To = dfrac(Num, instead(sqrt, S2P * Ns, sqrt(S2P * Ns))).
+    From = sqrt(S2P * Ns),
+    To = instead(sqrt1, S2P * Ns, sqrt(S2P * Ns)).
 
 feedback(sqrt1, [S2P_Ns], Col, FB)
  => FB = [ "The square root around ", \mmlm(Col, color(sqrt1, S2P_Ns)), " ",
@@ -225,10 +225,10 @@ hint(sqrt1, [_], Col, FB)
 % Forget square root around sample size
 buggy(stage(2), From, To, [step(buggy, sqrt2, [Ns])]) :-
     Ns = frac(1, _N_VR) + frac(1, _N_Box),
-    From = dfrac(Num, sqrt(S2P * Ns)),
-    To = dfrac(Num, invent_right(sqrt2, sqrt(omit_right(sqrt2, S2P * Ns)) * Ns)).
+    From = sqrt(S2P * Ns),
+    To = invent_right(sqrt2, sqrt(omit_right(sqrt2, S2P * Ns)) * Ns).
 
-feedback(nosr2, [Ns], Col, FB)
+feedback(sqrt2, [Ns], Col, FB)
  => FB = [ "The square root seems to stop ",
            "before ", \mmlm(Col, [paren(color(sqrt2, Ns)), "."])
          ].
