b4f23c947b04adba4bcb70434a5d8114d313089f
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2022-10-20 13:41:30 +0200

    Create cipaired.R

---
+++ cipaired.R
@@ -0,0 +1,29 @@
+# Raw data
+n     <- as.integer(round(runif(1, min=20, max=45)))
+Id    <- 1:n
+T0    <- round(runif(n, min=15, max=40))
+EOT   <- round(T0 + runif(n, min=-10, max=2))
+data  <- data.frame(Id, T0, EOT)
+
+# Summary statistics
+mu    <- round(runif(1, min=2, max=5), 1)
+t0    <- mean(data$T0)
+s_t0  <- sd(data$T0)
+eot   <- mean(data$EOT)
+s_eot <- sd(data$EOT)
+
+d1    <- mean(data$T0 - data$EOT)
+# d is a range of possible values
+d     <- call('...', floor(d1*10)/10, ceiling(d1*10)/10)
+
+s1_d  <- sd(data$T0 - data$EOT)
+s_d   <- call('...', floor(s1_d*10)/10, ceiling(s1_d*10)/10)
+
+tails <- "two-tailed"
+alpha <- 0.05
+
+# Export data for download
+download <- function(fname)
+{
+    write.csv2(data, fname, row.names=FALSE)
+}
