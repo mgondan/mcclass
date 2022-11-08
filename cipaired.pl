f06e035b41a5f0205960f30e235c8b9466dc794e
mgondan <Matthias.Gondan@psy.ku.dk>
2022-11-08 19:33:33 +0100

    few more buggy rules

--- cipaired.pl
+++ cipaired.pl
@@ -69,6 +69,7 @@ render(item(_T0, _S_T0, _EOT, _S_EOT, _D, _S_D, N, _Mu, _Alpha), Form) -->
         \htmlform(["Determine the confidence interval for the change in participants' self-confidence. The alpha level is ", \mmlm(alpha = perc(0.05))
                 ], '#cipaired', R)
     ]).
+ %Sollte noch eine Vorgabe für die Eingabe in der Aufgabenstellung angegeben werden?
 
 % t-test and confidence intervall for paired samples 
 intermediate(item).
@@ -83,8 +84,8 @@ expert(stage(2), X, Y, [step(expert, paired, [])]) :-
 
 feedback(paired, [], Col, F)
  => F = [ "Correctly recognised the problem as ",
-          "a ", \mmlm(Col, hyph(t, "test")), " for paired samples and the ",
-          "confidence interval for a mean value." 
+          "a ", \mmlm(Col, hyph(t, "test")), " for paired samples and that ",
+          "a confidence interval for the mean value has to be calculated." 
         ].
 
 hint(paired, [], Col, H)
@@ -98,74 +99,116 @@ expert(stage(2), X, Y, [step(expert, ci_lower, [D, S_D, N, Alpha])]) :-
     X = paired(D, Mu, S_D, N, Alpha),
     Y = hdrs(pm(D, quant(D, Mu, S_D, N, Alpha) * frac(S_D, sqrt(N)))).
 
-feedback(ci_lower, [_D, _S_D, _N, _Alpha], Col, F)
- => F = [ "Correctly identified the ", \mmlm(Col, hyph(t, "ratio")), " for ",
-           "paired samples and the confidence interval for a mean value." 
+feedback(ci_lower, [_D, _S_D, _N, _Alpha], _Col, F)					
+ => F = [ "Correctly identified the formula for the upper and lower bound of ",
+           "the confidence interval for a mean value." 
         ].
 
 hint(ci_lower, [D, S_D, N, Alpha], Col, H)
- => H = [ "The lower bound of the confidence interval ",
-          "is ", \mmlm(Col, D + qt(Alpha/2, N-1) * frac(S_D, sqrt(N)))
+ => H = [ "The formula to calculate the lower and upper bound of the ",
+          "confidence interval is ",
+	  \mmlm(Col, pm(D, qt(1 - Alpha/2, N-1) * frac(S_D, sqrt(N))))
         ].
 
 % Third step: Choose the correct quantile of the t-distribution
 expert(stage(2), X, Y, [step(expert, tquant, [N, Alpha])]) :-
     X = quant(_D, _Mu, _S_D, N, Alpha),
-    Y = qt(1-Alpha/2, N-1).
+    Y = qt(1 - Alpha/2, N-1).
 
 feedback(tquant, [_N, Alpha], Col, F)
- => F = [ "Correctly used the ", \mmlm(Col, hyph(1-Alpha/2, "quantile")),
+ => F = [ "Correctly used the ", \mmlm(Col, hyph(1 - Alpha/2, "quantile")),
           "of the ", \mmlm(Col, hyph(t, "distribution"))
         ].
 
 hint(tquant, [_N, Alpha], Col, H)
- => H = [ "Make sure to use the ", \mmlm(Col, hyph(1-Alpha/2, "quantile")),
+ => H = [ "Make sure to use the ", \mmlm(Col, hyph(1 - Alpha/2, "quantile")),
           "of the ", \mmlm(Col, hyph(t, "distribution"))
         ].
 
 % Use t-statistic instead of t-quantile
 buggy(stage(2), X, Y, [step(buggy, tstat, [D, S_D, N, Mu, Alpha])]) :-
     X = quant(D, Mu, S_D, N, Alpha),
-    Y = dfrac(D - Mu, S_D / sqrt(N)). % abbreviation abbrev(s2p, var_pool(S_T0^2, N, S_EOT^2, N), "the pooled variance")
+    P = abbrev(t, dfrac(D - Mu, S_D / sqrt(N)), ["the observed", space, t, "-statistic."]),		%Ist es möglich, die t-statistik hier auch kursiv zu schrebien?
+    Y = P. 
 
 feedback(tstat, [_D, _S_D, _N, _Mu, _Alpha], Col, F)
- => F = [ "The result matches the confidence interval based on the observed",
-           " ", \mmlm(Col, hyph(t, "statistic.")), " Please use the quantile ",
-           "of the ", \mmlm(Col, hyph(t, "distribution")), "instead."
+ => F = [ "The result matches the confidence interval based on the observed ",
+          \mmlm(Col, hyph(t, "statistic.")), " Please use the quantile ",
+           "of the ", \mmlm(Col, hyph(t, "distribution")), " instead."
         ].
 
-hint(tstat, [_D, _S_D, _N, _Mu, _Alpha], _Col, H)
- => H = [ "Do not insert the t-statistic into the expression for the ",
-          "confidence interval. Use the quantile of the t-distribution instead."
+hint(tstat, [_D, _S_D, _N, _Mu, _Alpha], Col, H)
+ => H = [ "Do not insert the ", \mmlm(Col, hyph(t, "statistic ")), "into the ",
+          "formula for the confidence interval. Use the quantile of the ",
+	   \mmlm(Col, hyph(t, "distribution")), " instead."
         ].
 
 % Buggy-Rule: Use z-quantile instead of t-quantile. This rule may be dropped
 % because we might not be able to distinguish the results.
 buggy(stage(2), X, Y, [step(buggy, qt, [N, Alpha])]) :-
-    X = qt(1-Alpha/2, N-1),
-    Y = instead(qt, qnorm(1-Alpha/2) , qt(1-Alpha/2, N-1)).
+    X = quant(_D, _Mu, _S_D, N, Alpha),
+    Y = instead(qt, qnorm(1 - Alpha/2) , qt(1 - Alpha/2, N - 1)).
 
 feedback(qt, [N, Alpha], Col, F)
- => F = [ "Please insert the quantile of the t-statistic ",
-          \mmlm(Col, color(qt, qt(1-Alpha/2, N-1))), " into ",
-          "the ", \mmlm(Col, hyph(t, "ratio.")) % not t-ratio, please change
+ => F = [ "The result matches the confidence interval based on the standard ",
+          "Normal distribution. ",
+          "Please insert the quantile of the ", \mmlm(Col, hyph(t, "distribution")),
+          \mmlm(Col, color(qt, qt(1 - Alpha/2, N - 1))), " into ",
+          "the formula for the confidence interval."
         ].
 
 hint(qt, [_N, _Alpha], Col, H)
- => H = [ "Do not insert the t-statistic into the ",
-          \mmlm(Col, hyph(t, "ratio.")), " Use the quantile of the ", % see above
-          "distribution instead."
+ => H = [ "Do not insert the quantile of the ", \mmlm(Col, hyph(z, "distribution ")), 
+          "into the formula for the confidence interval. Use the quantile of the ", 			
+	  \mmlm(Col, hyph(t, "distribution")), "instead."
         ].
 
-% 1. Bitte Feedback verschoenern. Hier viel Energie reinstecken, pädagogisch sinnvoll
-% 2. Abkürzung für t-Statistik, evtl. auch für t-Quantil (wahrscheinlich nicht)
+
 % 3. Buggy rule SPSS
-% 4. Dritten Trap suchen, damit er angezeigt wird
-% 5. Wurzel vergessen
 % 6. in tpaired-celina reinschauen, ob Ihr noch irgendwelche bugs findet/dort noch irgendwelche stehen: https://www.dropbox.com/scl/fo/4npx22e6wowo4zad3riau/h?dl=0&rlkey=51k1yq6a6u66uef5vf727qmgz
 
 % buggy rule/spss: mu
-%expert(stage(2), X, Y, [step(expert, ci_lower, [D, S_D, N, Alpha])]) :-
-%    X = paired(D, S_D, N, Alpha),
-%    Y = hdrs(D - Mu - qt(Alpha/2, N-1) * S_D / sqrt(N)).
+buggy(stage(2), X, Y, [step(buggy, spss, [Mu])]) :-
+    X = paired(D, Mu, S_D, N, Alpha),
+    Y = hdrs(pm(invent_right(spss, D - Mu), quant(D, Mu, S_D, N, Alpha) * frac(S_D, sqrt(N)))).
 
+feedback(spss, [Mu], Col, F)
+ => F = [ "The result matches...(bitte weiter) Please do not forget to add ", \mmlm(Col, Mu), " to the upper and lower bound",
+	  " of the confidence interval, if you calculate the bounds with SPSS."
+        ].
+
+hint(spss, [Mu], Col, H)
+ => H = [ "If you calculate the bounds of the confindence intervall with SPSS ",
+	  "keep in mind, that SPSS subtracts", \mmlm(Col, Mu), " from the two ",
+          "bounds of the CI (which must be undone)."
+        ].
+
+% Buggy-Rule: Use of N instead of sqrt(N)				% Durch Buggy-Rule mit t-statistik zweimal Fehler mit Wurzel vergessen möglich,
+buggy(stage(2), X, Y, [step(buggy, sqrt1, [N])]) :-			% sollte dies im Text oder farblich unterschieden werden?
+    X = quant(D, Mu, S_D, N, Alpha) * frac(S_D, sqrt(N)),
+    Y = quant(D, Mu, S_D, N, Alpha) * frac(S_D, omit_right(sqrt1, (2*N)^(1/2))).
+
+feedback(sqrt1, [N], Col, FB)
+ => FB = [ "Please do not forget the square root around ", 
+           \mmlm(Col, color(sqrt1, N))
+         ].
+
+hint(sqrt1, [N], Col, FB)
+ => FB = [ "Do not forget the square root around ",
+           \mmlm(Col, color(sqrt1, N))
+	     ].
+
+% Same mistake in the t-ratio
+buggy(stage(2), X, Y, [step(buggy, sqrt2, [N])]) :-
+    X = dfrac(D - Mu, S_D / sqrt(N)),
+    Y = dfrac(D - Mu, S_D / omit_right(sqrt2, (3*N)^(1/2))).
+
+feedback(sqrt2, [N], Col, FB)
+ => FB = [ "Please do not forget the square root around ",
+           \mmlm(Col, color(sqrt2, N))
+         ].
+
+hint(sqrt2, [N], Col, FB)
+ => FB = [ "Do not forget the square root around ",
+           \mmlm(Col, color(sqrt2, N))
+         ]. 
