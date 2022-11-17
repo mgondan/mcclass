21c850bc1f1935fdf86f77b875652118739c4f8f
mgondan <Matthias.Gondan@psy.ku.dk>
2022-11-17 08:43:55 +0100

    minor cleanup

--- powbinom.R
+++ powbinom.R
@@ -4,25 +4,25 @@ p0    = 0.6
 p1    = 0.8
 k     = NA
 
-uqbinom = function(...)
+uqbinom <- function(...)
   as.integer(qbinom(..., lower.tail=FALSE) + 1)
 
-lqbinom = function(...)
+lqbinom <- function(...)
   as.integer(qbinom(..., lower.tail=TRUE) - 1)
 
-udbinom = function(Alpha, N, Pi)
+udbinom <- function(Alpha, N, Pi)
 {
   v = dbinom(round(N*Pi):N, N, Pi)
   N - sum(v <= Alpha) + 1L
 }
 
-ldbinom = function(Alpha, N, Pi)
+ldbinom <- function(Alpha, N, Pi)
 {
   v = dbinom(0L:round(N*Pi), N, Pi)
   sum(v <= Alpha) - 1L
 }
 
-cbinom = function(Alpha, N, Pi, Tail, Arg)
+cbinom <- function(Alpha, N, Pi, Tail, Arg)
 {
   if(Tail == "upper" & Arg == "min")
     return(uqbinom(Alpha, N, Pi))
@@ -39,12 +39,12 @@ cbinom = function(Alpha, N, Pi, Tail, Arg)
   stop("Wrong Tail or Arg")
 }
 
-tail1 = tail2 = function(Tail, K)
+tail1 <- tail2 <- function(Tail, K)
 {
   return(Tail)
 }
 
-arg = function(Arg, K)
+arg <- function(Arg, K)
   return(Arg)
 
 pwbinom <- function(Crit, N, P1, Tail)
