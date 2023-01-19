f83a123b823340459f32369bf987b3c7dc83e4e9
mgondan <Matthias.Gondan@psy.ku.dk>
2023-01-19 11:43:07 +0100

    subtask

--- qbinom.pl
+++ qbinom.pl
@@ -9,8 +9,9 @@
 
 :- use_module(navbar).
 navbar:page(qbinom, "Binomial test").
+task(amountsuccess).
 
-:- discontiguous intermediate/1, expert/4, buggy/4, feedback/4, hint/4.
+:- discontiguous intermediate/2, expert/5, buggy/5, feedback/4, hint/4.
 
 mathml_hook(p0, sub(pi, 0)).
 mathml_hook(n, 'N').
@@ -27,53 +28,47 @@ rint:r_hook(cbinom(_Alpha, _Size, _Prob, _Tail, _Arg)).
 rint:r_hook(pbinom(_Q, _Size, _Prob)).
 rint:r_hook(pbinom(_Q, _Size, _Prob, _Tail)).
 
-render(item(Alpha, N, P0), Form) -->
-    { option(resp(R), Form, '#'),
-      binomtable(N, P0, Caption, Rows, Cols, Cells)
-    }, 
+render
+--> { start(item(_Alpha, N, P0)), 
+      binomtable(N, P0, Caption, Rows, Cols, Cells) 
+    },
     html(
       [ div(class(card), div(class('card-body'),
-          [ h1(class('card-title'), "Binary outcomes"),
-            p(class('card-text'), 
-              [ "Consider a clinical study with ", \mmlm(n = r(N)), " ",
-                "patients. The variable ", \mmlm('X'), " represents the ",
-                "number of therapeutic successes in the sample. We assume ",
-                "that the successes occur independently, and under the null ",
-                "hypothesis, the success probability is ", \mmlm(r(P0)), " ",
-                "in all patients. The binomial probabilities are given in ",
-                "the table below."
-	          ]),
-	        div(class(container),
-	          div(class("row justify-content-md-center"),
-	            div(class("col-6"),
-	              \htmltable(Caption, Rows, Cols, Cells))))
-	      ])),
-        div(class(card), div(class('card-body'),
-          [ h4(class('card-title'), [a(id(question), []), 
-              "Question"]),
-            p(class('card-text'),
-              [ "How many successes are needed to rule out the null ",
-                "hypothesis at the one-tailed significance level ",
-                "of ", \mmlm(alpha = r(Alpha)), "?"
-              ]), 
-            form([class(form), method('POST'), action('#dbinom-dbinom')],
-              [ div(class("input-group mb-3"),
-                  [ div(class("input-group-prepend"), 
-                      span(class("input-group-text"), "Response")),
-                    input([class("form-control"), type(text), name(resp), value(R)]),
-                      div(class("input-group-append"),
-                        button([class('btn btn-primary'), type(submit)], "Submit"))
-                  ])
-              ])
-          ]))
+        [ h1(class('card-title'), "Binary outcomes"),
+          p(class('card-text'), 
+            [ "Consider a clinical study with ", \mmlm(n = r(N)), " ",
+              "patients. The variable ", \mmlm('X'), " represents the ",
+              "number of therapeutic successes in the sample. We assume ",
+              "that the successes occur independently, and under the null ",
+              "hypothesis, the success probability is ", \mmlm(r(P0)), " ",
+              "in all patients. The binomial probabilities are given in ",
+              "the table below."
+            ]),
+		  div(class(container),
+	        div(class("row justify-content-md-center"),
+              div(class("col-6"), \htmltable(Caption, Rows, Cols, Cells))))
+        ]))
       ]).
 
-intermediate(item).
+task(amountsuccess, Form)
+--> { start(item(Alpha, _N, _P0)),
+     (   option(task(amountsuccess), Form)
+      ->  option(resp(Resp), Form, '#'),
+          session_retractall(resp(qbinom, amountsuccess, _)),
+          session_assert(resp(qbinom, amountsuccess, Resp))
+      ;   session_data(resp(qbinom, amountsuccess, Resp), resp(qbinom, amountsuccess, '#'))
+      )
+	},
+	html(\htmlform([ "How many successes are needed to rule out the null ",
+        "hypothesis at the one-tailed significance level ",
+        "of ", \mmlm(alpha = r(Alpha)), "?"], amountsuccess, Resp)).
+
+intermediate(amountsuccess, item).
 start(item(alpha, n, p0)).
 
 % This is a problem that involves the binomial test 
-intermediate(binom).
-expert(stage(2), From, To, [step(expert, problem, [])]) :-
+intermediate(amountsuccess, binom).
+expert(amountsuccess, stage(2), From, To, [step(expert, problem, [])]) :-
     From = item(Alpha, N, P0),
     To   = binom(Alpha, N, P0).
 
@@ -84,7 +79,7 @@ hint(problem, [], _Col, Hint) =>
     Hint = [ "This problem involves the binomial test." ].
 
 % Upper tail of the binomial distribution
-expert(stage(2), From, To, [step(expert, upper, [])]) :-
+expert(amountsuccess, stage(2), From, To, [step(expert, upper, [])]) :-
     From = binom(Alpha, N, P0),
     To   = binom(Alpha, N, P0, tail("upper", k), arg("min", k > N*P0)).
 
@@ -95,7 +90,7 @@ hint(upper, [], _Col, Hint)
  => Hint = [ "The upper tail of the binomial distribution is needed." ].
 
 % Lower tail of the binomial distribution
-buggy(stage(2), From, To, [step(buggy, lower, [])]) :-
+buggy(amountsuccess, stage(2), From, To, [step(buggy, lower, [])]) :-
     From = binom(Alpha, N, P0),
     To   = binom(Alpha, N, P0, instead(lower, tail("lower", k), tail("upper", k)), instead(lower, arg("max", k < N*P0), arg("min", k > N*P0))).
 
@@ -106,7 +101,7 @@ hint(lower, [], _Col, Hint)
  => Hint = [ "Do not select the lower tail of the binomial distribution." ].
 
 % Critical value based on distribution
-expert(stage(2), From, To, [step(expert, dist, [])]) :-
+expert(amountsuccess, stage(2), From, To, [step(expert, dist, [])]) :-
     From = binom(Alpha, N, P0, Tail, Arg),
     To   = cbinom(Alpha, N, P0, Tail, Arg).
 
@@ -121,7 +116,7 @@ hint(dist, [], _Col, Hint)
            ].
 
 % Critical value based on density
-buggy(stage(3), From, To, [step(buggy, dens, [K])]) :-
+buggy(amountsuccess, stage(3), From, To, [step(buggy, dens, [K])]) :-
     From = tail(Tail, K),
     member(Tail, ["upper", "lower"]),
     To = instead(dens, tail("equal", K), tail("upper", K)).
@@ -140,8 +135,8 @@ hint(dens, [_K], _Col, Hint)
 
 % Helper function(s)
 binomtable(N, P0, Caption, Rows, Cols, Cells) :-
-    r_task(lqbinom(0.05, N, P0), L),
-    r_task(uqbinom(0.05, N, P0), H),
+    r_topic(lqbinom(0.05, N, P0), L),
+    r_topic(uqbinom(0.05, N, P0), H),
     Caption = [em("Table 1. "), "Binomial probabilities"],
     Cols = [\mmlm(k), \mmlm(dbinom(k, n = r(N), p0 = r(P0)))],
     % lower tail
