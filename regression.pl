293eec50b73b845594143de99772f895a12ffd21
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2025-03-29 20:32:23 +0100

    Fix and complete task regression

--- regression.pl
+++ regression.pl
@@ -11,159 +11,167 @@
 navbar:page(regression, ["regression"]).
 
 task(bcoef).
-task(correlation).
 task(pvalue).
+label(bcoef, "Estimate").
+label(pvalue, [math(mi(p)), "-value"]).
 
-label(bcoef, "bcoef").
-label(correlation, "correlation").
-label(pvalue, "pvalue").
-
-:- discontiguous intermediate/2, expert/5, buggy/5, feedback/4, hint/4, r_hook/1.
+:- discontiguous intermediate/2, expert/5, buggy/5, feedback/4, hint/4.
 
 % Prettier symbols for mathematical rendering
-
-math_hook(n, 'N').
+mathml:math_hook(n, 'N').
+mathml:math_hook(lm0(Y, X, "intercept"), ["Intercept: ", r(Y), "~", r(X)]).
+mathml:math_hook(lm0(Y, X, "coef"), ["Estimate: ", r(Y), "~", r(X)]).
+mathml:math_hook(lm0(Y, X, "pval:coef"), ["Estimate", p, "-value: ", r(Y), "~", r(X)]).
+mathml:math_hook(lm0(Y, X, "pval:intercept"), ["Intercept", p, "-value: ", r(Y), "~", r(X)]).
 
 % R definitions
-
 r_hook(n).
-r_hook(mean_Sleep).
-r_hook(sd_Sleep).
-r_hook(mean_Dep).
-r_hook(sd_Dep).
-r_hook(dep).
-r_hook(sleep).
+r_hook(y).
+r_hook(x).
 
-r_hook(bcoef/2).
+r_hook(lm0/3).
+mono(lm0/3, [/, /, /]).
 
 % Task description
-
 render
---> { start(item(N, _Mean_Sleep, _SD_Sleep, _Mean_Dep, _SD_Dep, _Dep, _Sleep)) },
+--> { start(item(N, _Y, _X)) },
 	html(
       [ div(class(card),
           div(class('card-body'),
-            [ h1(class('card-title'), "Linear regression"),
+            [ h1(class('card-title'), "Sleep and Depression"),
 		      p(class('card-text'),
                 [ "Consider a clinical study researching the relationship between sleep duration ",
-                "(X, in hours per night) and depression severity ", 
-                "(Y, measured on a standardized scale) in a sample of patients ",
+                "(hours per night) and depression severity measured on the Hamilton Rating Scale for Depression ", 
+                "(HDRS, range from best = 0 to worst = 42) in a sample of patients (",
                 \mmlm(N = r(N)),
-                " diagnosed with major depressive disorder."
+                "). The dataset can be downloaded below."
                 ]),
           \download(regression)
           ]))]).
 
 % Question for b-coefficient
-
 task(bcoef)
---> { start(item(_N, _Mean_Sleep, _SD_Sleep, _Mean_Dep, _SD_Dep, _Dep, _Sleep)),
+--> { start(item(_N, _Y, _X)),
       session_data(resp(regression, bcoef, Resp), resp(regression, bcoef, '#.##'))
     },
-	html(\htmlform(["Calculate the b-coefficient for the sleep duration."], bcoef, Resp)).
+	html(\htmlform(["How does the depression severity change (increase/decrease in HDRS score) for every hour of additional sleep?"], bcoef, Resp)).
 
+% Question for p-value 
+task(pvalue)
+--> { start(item(_N, _Y, _X)),
+      session_data(resp(regression, pvalue, Resp), resp(regression, pvalue, '#.##'))
+    },
+	html(\htmlform(["What is the ", span(class('text-nowrap'), [\mmlm(p), "-value"]), " of the estimate?"], pvalue, Resp)).
+
+%
 % Expert rule for b-coefficient
-
+%
+start(item(n, y, x)).
 intermediate(bcoef, item).
-start(item(n, mean_Sleep, sd_Sleep, mean_Dep, sd_Dep, dep, sleep)).
 
-% First step
+% First step: recognize the problem
+intermediate(bcoef, problem).
+expert(bcoef, stage(1), From, To, [step(expert, problem, [])]) :-
+    From = item(_N, Y, X),
+    To = { '<-'(b, linearmodel(Y, X)) }.
 
-intermediate(bcoef, correct).
-expert(bcoef, stage(2), X, Y, [step(expert, correct, [])]) :-
-    X = item(_N, _Mean_Sleep, _SD_Sleep, _Mean_Dep, _SD_Dep, Dep, Sleep),
-    Y = { '<-'(b, regression0(Dep, Sleep)) }.
+feedback(problem, [], _Col, F)
+ => F = ["Correctly recognised the problem as a linear regression."].
 
-feedback(correct, [], _Col, F)
- => F = ["Test1"].
+hint(problem, [], _Col, F)
+ => F = ["This is a linear regression."].
 
-hint(correct, [], _Col, F)
- => F = ["Test1"].
+% Second step: extract coefficient from linear model
+intermediate(bcoef, linearmodel).
+expert(bcoef, stage(1), From, To, [step(expert, linearmodel, [Y, X])]) :-
+    From = linearmodel(Y, X),
+    To = lm0(Y, X, "coef").
 
-% Second step
+feedback(linearmodel, [_,_], _Col, F)
+ => F = ["Correctly extracted the ", span(class('text-nowrap'), [\mmlm(b), "-coefficient for the sleep duration."])].
 
-expert(bcoef, stage(2), X, Y, [step(expert, bcoef, [Dep, Sleep])]) :-
-    X = correct(Dep, Sleep),
-    Y = bcoef(Dep, Sleep).
+hint(linearmodel, [_,_], _Col, F)
+ => F = ["Report the coefficient for the sleep duration."].
 
-feedback(bcoef, [_,_], _Col, F)
- => F = ["Test2"].
 
-hint(bcoef, [_,_], _Col, F)
- => F = ["Test2"].
+%
+% Buggy-Rules for b-coefficient task
+%
 
-% Buggy rules for b-coefficient
-/*
-% Switched dependent and independent variable
-
-buggy(bcoef, stage(1), X, Y, [step(buggy, switch, [Dep, Sleep])]) :-
-    X = regression0(Dep, Sleep),
-    Y = regression1(instead(switch, (Sleep, Dep), (Dep, Sleep))).
+% Buggy-Rule: switched outcome and predictor
+buggy(bcoef, stage(1), From, To, [step(buggy, switch, [Y, X])]) :-
+    From = linearmodel(Y, X),
+    To = instead(switch, lm0(X, Y, "coef"), lm0(Y, X, "coef")).
 
 feedback(switch, [_, _], _, F)
- => F = ["Test"].
+ => F = ["The predictor and outcome variable of the model were switched."].
 
 hint(switch, [_,_], _, F)
- => F = ["Test"].
-*/
+ => F = ["Make sure to define the correct outcome variable and predictor in the model."].
 
-/*
-% Expert rule for correlation
+% Buggy-Rule: reported intercept instead of predictor 
+buggy(bcoef, stage(1), From, To, [step(buggy, intercept, [Y, X])]) :-
+    From = lm0(X, Y, "coef"),
+    To = lm0(Y, X, "intercept").
 
-intermediate(correlation, item).
+feedback(intercept, [_, _], _, F)
+ => F = ["The intercept was reported."].
 
-% First step
-
-intermediate(correlation, correct).
-expert(correlation, stage(2), X, Y, [step(expert, correct, [])]) :-
-    X = item(_N, _Mean_Sleep, _SD_Sleep, _Mean_Dep, _SD_Dep, _B_Coef, _Intercept, Cor_Value, _P_Value),
-    Y = { '<-'(c, correct(Cor_Value)) }.
-
-feedback(correct, [], _Col, F)
- => F = ["Test1"].
-
-hint(correct, [], _Col, F)
- => F = ["Test1"].
-
-% Second step
-
-expert(correlation, stage(2), X, Y, [step(expert, correlation, [Cor_Value])]) :-
-    X = correct(Cor_Value),
-    Y = Cor_Value.
-
-feedback(correlation, [_], _Col, F)
- => F = ["Test2"].
-
-hint(correlation, [_], _Col, F)
- => F = ["Test2"].
+hint(intercept, [_,_], _, F)
+ => F = ["Make sure to report the coefficient of the predictor and not of the intercept."].
 
 
+%
 % Expert rule for p-value
-
+%
 intermediate(pvalue, item).
 
-% First step
+% First step: recognize the problem
+intermediate(pvalue, problem).
+expert(pvalue, stage(2), From, To, [step(expert, problem, [])]) :-
+    From = item(_N, Y, X),
+    To = { '<-'(p, linearmodel(Y, X)) }.
 
-intermediate(pvalue, correct).
-expert(pvalue, stage(2), X, Y, [step(expert, correct, [])]) :-
-    X = item(_N, _Mean_Sleep, _SD_Sleep, _Mean_Dep, _SD_Dep, _B_Coef, _Intercept, _Cor_Value, P_Value),
-    Y = { '<-'(p, correct(P_Value)) }.
+feedback(problem, [], _Col, F)
+ => F = ["Correctly recognised the problem as a linear regression."].
 
-feedback(correct, [], _Col, F)
- => F = ["Test1"].
+hint(problem, [], _Col, F)
+ => F = ["This is a linear regression."].
 
-hint(correct, [], _Col, F)
- => F = ["Test1"].
+% Second step: extract p-value of coefficient from linear model
+intermediate(pvalue, linearmodel).
+expert(pvalue, stage(2), From, To, [step(expert, getpvalue, [Y, X])]) :-
+    From = linearmodel(Y, X),
+    To = lm0(Y, X, "pval:coef").
 
-% Second step
+feedback(getpvalue, [_Y, _X], _Col, F)
+ => F = ["Correctly extracted the ", span(class('text-nowrap'), [\mmlm(p), "-value"]), " for the estimate of the sleep duration."].
 
-expert(pvalue, stage(2), X, Y, [step(expert, pvalue, [P_Value])]) :-
-    X = correct(P_Value),
-    Y = P_Value.
+hint(getpvalue, [_Y, _X], _Col, F)
+ => F = ["Report the ", span(class('text-nowrap'), [\mmlm(p), "-value"]), " for the estimate of the sleep duration."].
 
-feedback(pvalue, [_], _Col, F)
- => F = ["Test2"].
+%
+% Buggy-Rules for p-value task
+%
 
-hint(pvalue, [_], _Col, F)
- => F = ["Test2"].
- */
+% Buggy-Rule: switched outcome and predictor
+buggy(pvalue, stage(2), From, To, [step(buggy, switch1, [Y, X])]) :-
+    From = linearmodel(Y, X),
+    To = instead(switch1, lm0(X, Y, "pval:coef"), lm0(Y, X, "pval:coef")).
+
+feedback(switch1, [_, _], _, F)
+ => F = ["The predictor and outcome variable of the model were switched."].
+
+hint(switch1, [_,_], _, F)
+ => F = ["Make sure to define the correct outcome variable and predictor in the model."].
+
+% Buggy-Rule: reported intercept instead of predictor 
+buggy(pvalue, stage(2), From, To, [step(buggy, intercept1, [Y, X])]) :-
+    From = lm0(Y, X, "pval:coef"),
+    To = lm0(Y, X, "pval:intercept").
+
+feedback(intercept1, [_, _], _, F)
+ => F = ["The ", span(class('text-nowrap'), [\mmlm(p), "-value"]), " of the intercept was reported."].
+
+hint(intercept1, [_,_], _, F)
+ => F = ["Make sure to report the ", span(class('text-nowrap'), [\mmlm(p), "-value"]), " for predictor and not for the intercept."].
