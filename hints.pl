23f9c47bb204a5f8f8c394d68d4d9e4acf1de355
mgondan <Matthias.Gondan@psy.ku.dk>
2025-04-25 13:40:47 +0200

    minor improvements

--- hints.pl
+++ hints.pl
@@ -15,7 +15,7 @@ hints(Topic, Task, Hints) :-
     maplist(hints, Solutions, Hints).
 
 % pretty print the set of hints for one given result
-pp_hint1(Topic, Task, _Data, Expr-Result/Flags)
+pp_hint(Topic, Task, _Data, Expr-Result/Flags, 1)
 --> { colors(Expr, Col),
       hints(Expr-Result/Flags, Hints),
       findall(li(H),
@@ -26,27 +26,14 @@ pp_hint1(Topic, Task, _Data, Expr-Result/Flags)
     },
     html(div(class('accordion-item'),
       [ h2(class('accordion-header'),
-          button([class('accordion-button'), type(button)],
-            \mmlm([topic(Topic), task(Task) | Col], Result))),
-        div(class('accordion-collapse collapse show'),
+          button([class('accordion-button bg-warning bg-opacity-50'), type(button), 'data-bs-toggle'(collapse),
+            'data-bs-target'('#collapse-~w-~w-1'-[Topic, Task]), 'aria-expanded'(true), 'aria-controls'('collapse-~w-~w-1'-[Topic, Task])],
+            "Result 1")),
+        div([class('accordion-collapse collapse show'), id('collapse-~w-~w-1'-[Topic, Task]), 'data-bs-parent'('#accordion-~w-~w'-[Topic, Task])],
           div(class('accordion-body'), ul(List)))
       ])).
 
-% pretty print the hints for all alternatives
-pp_hints1(Topic, Task, Data)
---> { member(solutions(Solutions), Data) },
-    html(div(class(card),
-      [ div(class('card-header text-white bg-info'), "Hints"),
-        div(class('card-body'),
-          [ p(class('card-text'), "Steps to the solution(s)"),
-            div(class('accordion accordion-flush'),
-              html(\foreach(member(S, Solutions),
-                html(\pp_hint(Topic, Task, Data, S)))))
-          ])
-      ])).
-
-% pretty print the set of hints for one given result
-pp_hint(Topic, Task, _Data, Expr-Result/Flags)
+pp_hint(Topic, Task, _Data, Expr-Result/Flags, Id)
 --> { colors(Expr, Col),
       hints(Expr-Result/Flags, Hints),
       findall(li(H),
@@ -55,18 +42,26 @@ pp_hint(Topic, Task, _Data, Expr-Result/Flags)
           Topic:hint(Name, Arg, [topic(Topic), task(Task) | Col], H)
         ), List)
     },
-    html(div([class(collapse), id('hint-~w-~w'-[Topic, Task])],
-        div(class('card card-body'), ul(List)))).
+    html(div(class('accordion-item'),
+      [ h2(class('accordion-header'),
+          button([class('accordion-button collapsed bg-warning bg-opacity-50'), type(button), 'data-bs-toggle'(collapse),
+            'data-bs-target'('#collapse-~w-~w-~w'-[Topic, Task, Id]), 'aria-expanded'(false), 'aria-controls'('collapse-~w-~w-~w'-[Topic, Task, Id])],
+            "Result ~w"-[Id])),
+        div([class('accordion-collapse collapse'), id('collapse-~w-~w-~w'-[Topic, Task, Id]), 'data-bs-parent'('#accordion-~w-~w'-[Topic, Task])],
+          div(class('accordion-body'), ul(List)))
+      ])).
 
 pp_hints(Topic, Task, Data)
 --> { member(solutions(Solutions), Data),
-      [ Sol1 | _ ] = Solutions
+      findall(I-S, nth1(I, Solutions, S), Enum)
     },
     html(div(class('card card-body'),
-      [ p(button([class('btn btn-primary'), type(button),
+      [ p(button([class('btn btn-warning'), type(button),
           'data-bs-toggle'(collapse), 'data-bs-target'('#hint-~w-~w'-[Topic, Task]), 
           'aria-expanded'(false), 'aria-controls'('hint-~w-~w'-[Topic, Task])],
           "Show hints")),
-        html(\pp_hint(Topic, Task, Data, Sol1))
+        div([class('card collapse'), id('hint-~w-~w'-[Topic, Task])],
+          div([class(accordion), id('accordion-~w-~w'-[Topic, Task])],
+            \foreach(member(I-S, Enum),
+              html(\pp_hint(Topic, Task, Data, S, I)))))
       ])).
-
