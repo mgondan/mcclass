9110fe2c21ca3848f93eabc5f99a674312d1c554
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2025-06-12 21:44:00 +0200

    Temporary solution to error

--- tgroups.pl
+++ tgroups.pl
@@ -380,8 +380,8 @@ intermediate(cigroups, indep).
 expert(cigroups, stage(1), X, Y, [step(expert, problem, [])]) :-
     X = item(VR, S_VR, N_VR, Box, S_Box, N_Box, Alpha),
     Y = { '<-'(s2p, var_pool(S_VR^2, N_VR, S_Box^2, N_Box)) ;
-          '<-'(ci, indep(VR, Box, s2p, N_VR, N_Box, Alpha))
-         }.
+          indep(VR, Box, s2p, N_VR, N_Box, Alpha)
+        }.
 
 feedback(problem, [], Col, F)
  => F = [ "Correctly recognised the problem as ",
