96094f4cab948b2cf4f9fdfa58c1da76c98bab1a
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-06-15 20:12:15 +0200

    Improve hints for single results

--- hints.pl
+++ hints.pl
@@ -10,12 +10,24 @@ hints(Topic, Task)
     html(Accordion).
 
 % Codes of correct steps
-init_hints(Topic, Task) :-
-    findall(sol(Task, Expr, Steps, Colors), Topic:sol(Task, Expr, Steps, Colors), Solutions),
+init_hints(Topic, Task, Solutions) :-
     nth1(Id, Solutions, sol(Task, Expr, Steps, Colors)),
-    init_hint(Topic, Task, Expr, Steps, Colors, Id).
+    init_hint(Topic, Task, Expr, Steps, Colors, Id, Solutions).
 
-init_hint(Topic, Task, Expr, Steps, Colors, 1) :-
+init_hint(Topic, Task, Expr, Steps, Colors, _, [_]) :-
+    findall(H, member(step(expert, H, _), Steps), Hints),
+    findall(li(Hint),
+      ( member(step(expert, H, Arg), Steps),
+        Topic:hint(H, Arg, [topic(Topic), task(Task) | Colors], Hint)
+      ), List),
+    AccItem = div(class('accordion-item'),
+      div([class('accordion-collapse collapse show'),
+          id('collapse-~w-~w-1'-[Topic, Task]),
+          'data-bs-parent'('#accordion-~w-~w'-[Topic, Task])],
+        div(class('accordion-body'), ul(List)))),
+    assert(Topic:hints(Task, Expr, Hints, AccItem)).
+
+init_hint(Topic, Task, Expr, Steps, Colors, 1, [_, _ | _]) :-
     findall(H, member(step(expert, H, _), Steps), Hints),
     findall(li(Hint),
       ( member(step(expert, H, Arg), Steps),
@@ -36,7 +48,7 @@ init_hint(Topic, Task, Expr, Steps, Colors, 1) :-
       ]),
     assert(Topic:hints(Task, Expr, Hints, AccItem)).
 
-init_hint(Topic, Task, Expr, Steps, Colors, Id) :-
+init_hint(Topic, Task, Expr, Steps, Colors, Id, [_, _ | _]) :-
     Id > 1,
     findall(H, member(step(expert, H, _), Steps), Hints),
     findall(li(Hint),
@@ -61,7 +73,8 @@ init_hint(Topic, Task, Expr, Steps, Colors, Id) :-
 % pretty print the set of hints for one given result
 init_hints(Topic) :-
     Topic:task(Task),
-    foreach(init_hints(Topic, Task), true),
+    findall(sol(Task, Expr, Steps, Colors), Topic:sol(Task, Expr, Steps, Colors), Solutions),
+    foreach(init_hints(Topic, Task, Solutions), true),
     findall(Hint-Item, Topic:hints(Task, _Expr, Hint, Item), Pairs),
     pairs_keys_values(Pairs, _Hints, AccItems),
     Accordion = div(class('card card-body'),
