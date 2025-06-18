97234d731191e5f9407a1d8956cdb3cba521d39d
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-06-18 08:39:28 +0200

    avoid use of tilde

--- regression.pl
+++ regression.pl
@@ -20,10 +20,10 @@ label(pvalue, [math(mi(p)), "-value"]).
 
 % Prettier symbols for mathematical rendering
 math_hook(n, 'N').
-math_hook(lm0(Y, X, "intercept"), ["Intercept: ", r(Y), "~", r(X)]).
-math_hook(lm0(Y, X, "coef"), ["Estimate: ", r(Y), "~", r(X)]).
-math_hook(lm0(Y, X, "pval:coef"), ["Estimate", p, "-value: ", r(Y), "~", r(X)]).
-math_hook(lm0(Y, X, "pval:intercept"), ["Intercept", p, "-value: ", r(Y), "~", r(X)]).
+math_hook(lm0(Y, X, "intercept"), ["Intercept: ", ~(r(Y), r(X))]).
+math_hook(lm0(Y, X, "coef"), ["Estimate: ", ~(r(Y), r(X))]).
+math_hook(lm0(Y, X, "pval:coef"), ["Estimate", p, "-value: ", ~(r(Y), r(X))]).
+math_hook(lm0(Y, X, "pval:intercept"), ["Intercept", p, "-value: ", ~(r(Y), r(X))]).
 
 % R definitions
 r_hook(n).
