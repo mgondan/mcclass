4315fa305d2096ad67d3bd6f1e75db64395d56cb
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2025-08-04 07:38:15 +0200

    Update interval wrapper

--- util.pl
+++ util.pl
@@ -1,4 +1,4 @@
-:- module(util, [nowrap//1, my_subset/3]).
+:- module(util, [nowrap//1, my_subset/3, assert_clauses/1, add_option/3]).
 
 :- use_module(library(http/html_write)).
 :- use_module(mathml).
@@ -14,3 +14,19 @@ my_subset([X | L], [X | S], D) :-
 my_subset(L, [H | S], [H | D]) :-
     my_subset(L, S, D).
 
+% Assert clauses of interval_/3 at beginning of module
+assert_clauses(Module) :-
+    findall((rint:interval_(Expr, Res, Flags) :- Body), 
+            clause(Module:interval_(Expr, Res, Flags), Body), Bag0),
+    reverse(Bag0, Bag),
+    maplist(asserta, Bag, _),
+    abolish(Module:interval_/3). 
+
+add_option(Option, Old, New),
+    compound_name_arguments(Option, Name, _),
+    OptionName =.. [Name, _],
+    option(OptionName, Old)
+ => New = Old.
+
+add_option(Option, Old, New)
+ => New = [Option | Old].
